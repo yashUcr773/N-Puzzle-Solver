731c9d3135118e8547f56d4dad8020d840981ab6
yashUcr773 <yagga004@ucr.edu>
2023-07-26 05:55:20 -0700

    test some routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { PlayerComponent } from './components/player/player.component';
 import { SolverComponent } from './components/solver/solver.component';
 import { MainComponent } from './components/main/main.component';
@@ -8,7 +7,7 @@ import { MainComponent } from './components/main/main.component';
 const routes: Routes = [
     {
         path: '', component: MainComponent, children: [
-            { path: '', redirectTo: 'Play', pathMatch: 'full' },
+            { path: '', redirectTo: '', pathMatch: 'full' },
             { path: 'Play', component: PlayerComponent },
             { path: 'Solve', component: SolverComponent }
         ]
