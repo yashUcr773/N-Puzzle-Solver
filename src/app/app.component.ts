c492bfccb352a6da3077a817e4f735d7e5146381
yashUcr773 <yagga004@ucr.edu>
2023-07-25 23:47:29 -0700

    added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { links } from './constants/links';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'NPuzzleSolverProject';
+export class AppComponent implements OnInit {
+
+    social_links = links;
+    constructor() {
+
+    }
+
+    
+    ngOnInit(): void {
+
+    }
+
+
+    launchLink(url: string, newTab = true) {
+        let link = document.querySelector('#external_link') as HTMLAnchorElement;
+        if (link) {
+            if (newTab) {
+                link.target = '_blank';
+            }
+            link.href = url;
+            link.click();
+        }
+    }
+
 }
