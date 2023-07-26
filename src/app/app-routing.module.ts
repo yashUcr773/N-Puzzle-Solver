2759f08896e10cf2bfb7d5ee882ba84d13260c94
yashUcr773 <yagga004@ucr.edu>
2023-07-26 14:10:19 -0700

    Revert "test some routing"
    This reverts commit 731c9d3135118e8547f56d4dad8020d840981ab6.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { PlayerComponent } from './components/player/player.component';
 import { SolverComponent } from './components/solver/solver.component';
 import { MainComponent } from './components/main/main.component';
@@ -7,7 +8,7 @@ import { MainComponent } from './components/main/main.component';
 const routes: Routes = [
     {
         path: '', component: MainComponent, children: [
-            { path: '', redirectTo: '', pathMatch: 'full' },
+            { path: '', redirectTo: 'Play', pathMatch: 'full' },
             { path: 'Play', component: PlayerComponent },
             { path: 'Solve', component: SolverComponent }
         ]
